af4bf141741617b970d1ab478dad291291cc97d8
Thalía Serrano Cañas <kkaalith@gmail.com>
2025-11-25 14:08:46 +0000

    comprobando

--- models/marts/finance/fact_billing.sql
+++ models/marts/finance/fact_billing.sql
@@ -1,8 +1,94 @@
-with base as (
-    select
-        id_bill,
-    from {{ ref('stg_hospital__billing') }}
+{{ config(
+    materialized = "incremental",
+) }}
+
+-- ============================================================
+-- FACT BILLING
+-- ============================================================
+-- Este modelo integra los datos ya normalizados en staging
+-- para construir la tabla de hechos principal del sistema.
+-- ============================================================
+
+WITH billing AS (
+    SELECT *
+    FROM {{ ref('stg_hospital__billing') }}
+),
+
+patients AS (
+    SELECT id_patient
+    FROM {{ ref('dim_patients') }}
+),
+
+doctors AS (
+    SELECT id_doctor
+    FROM {{ ref('dim_doctors') }}
+),
+
+payment_methods AS (
+    SELECT id_payment_method
+    FROM {{ ref('dim_payment_method') }}
+),
+
+payment_status AS (
+    SELECT id_payment_status
+    FROM {{ ref('dim_payment_status') }}
+),
+
+treatment_type AS (
+    SELECT id_treatment_type
+    FROM {{ ref('dim_treatment_type') }}
+),
+
+date_dim AS (
+    SELECT id_date
+    FROM {{ ref('dim_date') }}
+),
+
+-- ============================================================
+-- ENRIQUECER BILLING CON DIMENSIONES
+-- ============================================================
+
+final AS (
+    SELECT
+        b.id_bill
+        -- FKs a dimensiones
+        p.id_patient,
+        d.id_doctor,
+        t.id_treatment_type,
+        pm.id_payment_method,
+        ps.id_payment_status,
+        dd.id_date,
+
+        -- Métricas del hecho
+        b.amount_dolars,
+        b.payment_status,
+        b.is_payed,
+        b.payments_days
+        b.billing_delay_days,
+        b.is_late,
+        b.late_fee_dolars,
+        b.insurance_coverage_amount,
+        b.patient_payment_amount,
+        b.total_amount
+
+        -- Fechas
+        b.bill_date,
+        b.payment_date,
+        b.due_date
+
+
+    FROM billing b
+    LEFT JOIN dim_patients        p   ON p.id_patient = b.id_patient
+    LEFT JOIN dim_doctors         d   ON d.id_doctor = b.id_doctor
+    LEFT JOIN dim_treatment_type  t   ON t.treatment_type = b.treatment_type
+    LEFT JOIN dim_payment_methods pm  ON pm.payment_method = b.payment_method
+    LEFT JOIN dim_payment_status  ps  ON ps.payment_status = b.payment_status
+    LEFT JOIN dim_date        dd  ON dd.full_date = b.bill_date
+
+    --{% if is_incremental() %}
+      --WHERE b.updated_at > (SELECT MAX(updated_at) FROM {{ this }})
+    --{% endif %}
 )
 
-select *
-from base
+SELECT *
+FROM final;
---
+++ tests/singular/test_appointments_completed_has_checkin.sql
@@ -0,0 +1,9 @@
+-- -----------------------------------------------------------
+-- Verifica que todas las citas con status = 'Completed'
+-- tengan un check_in_time no nulo.
+-- -----------------------------------------------------------
+
+SELECT *
+FROM {{ source('hospital','appointments') }}
+WHERE status = 'Completed'
+  AND check_in_time IS NULL;
